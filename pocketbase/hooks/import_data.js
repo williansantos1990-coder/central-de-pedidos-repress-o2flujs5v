86d1eb058be4f01c0a6abb34cbd53c32e5f8b09e
Skip <skip@usecurling.com>
2026-07-14 13:31:56 -0300

    feat: Implement data import for PEDVE012, PEDVE005, Transportadoras

--- pocketbase/hooks/import_data.js
+++ pocketbase/hooks/import_data.js
@@ -1,11 +1,8 @@
-// @deps xlsx@0.18.5
 routerAdd(
   'POST',
   '/backend/v1/import',
   (e) => {
-    const XLSX = require('xlsx')
-
-    const userId = e.auth && e.auth.id
+    var userId = e.auth && e.auth.id
     if (!userId) return e.unauthorizedError('auth required')
 
     var collectionsConfig = [
@@ -27,10 +24,10 @@ routerAdd(
       return e.badRequestError('Nenhum arquivo enviado')
     }
 
-    function bytesToBinary(bytes) {
-      var result = ''
-      for (var i = 0; i < bytes.length; i++) {
-        result += String.fromCharCode(bytes[i])
+    function subarray(data, start, end) {
+      var result = []
+      for (var i = start; i < end && i < data.length; i++) {
+        result.push(data[i])
       }
       return result
     }
@@ -241,6 +238,423 @@ routerAdd(
       return val
     }
 
+    function decodeXmlEntities(str) {
+      return str
+        .replace(/&amp;/g, '&')
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+        .replace(/&quot;/g, '"')
+        .replace(/&apos;/g, "'")
+        .replace(/&#(\d+);/g, function (_, code) {
+          return String.fromCharCode(parseInt(code, 10))
+        })
+        .replace(/&#x([0-9a-fA-F]+);/g, function (_, code) {
+          return String.fromCharCode(parseInt(code, 16))
+        })
+    }
+
+    function inflateData(src) {
+      var dst = []
+      var si = 0
+      var bitBuf = 0
+      var bitCnt = 0
+
+      function bits(n) {
+        while (bitCnt < n) {
+          bitBuf |= src[si++] << bitCnt
+          bitCnt += 8
+        }
+        var v = bitBuf & ((1 << n) - 1)
+        bitBuf >>>= n
+        bitCnt -= n
+        return v
+      }
+
+      function buildTable(lengths) {
+        var maxLen = 0
+        for (var i = 0; i < lengths.length; i++) {
+          if (lengths[i] > maxLen) maxLen = lengths[i]
+        }
+        var blCount = []
+        for (var i = 0; i <= maxLen; i++) blCount.push(0)
+        for (var i = 0; i < lengths.length; i++) {
+          if (lengths[i] > 0) blCount[lengths[i]]++
+        }
+        var nextCode = [0]
+        var code = 0
+        for (var i = 1; i <= maxLen; i++) {
+          code = (code + blCount[i - 1]) << 1
+          nextCode.push(code)
+        }
+        var table = {}
+        for (var i = 0; i < lengths.length; i++) {
+          if (lengths[i] > 0) {
+            var c = nextCode[lengths[i]]++
+            table[lengths[i] * 65536 + c] = i
+          }
+        }
+        return { table: table, maxLen: maxLen }
+      }
+
+      function decode(t) {
+        var code = 0
+        for (var len = 1; len <= t.maxLen; len++) {
+          code = (code << 1) | bits(1)
+          if (t.table[len * 65536 + code] !== undefined) return t.table[len * 65536 + code]
+        }
+        return -1
+      }
+
+      var lenBase = [
+        3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31, 35, 43, 51, 59, 67, 83, 99, 115,
+        131, 163, 195, 227, 258,
+      ]
+      var lenExtra = [
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0,
+      ]
+      var distBase = [
+        1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537,
+        2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577,
+      ]
+      var distExtra = [
+        0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12,
+        13, 13,
+      ]
+
+      var fixedLit = []
+      for (var i = 0; i < 288; i++) {
+        if (i < 144) fixedLit.push(8)
+        else if (i < 256) fixedLit.push(9)
+        else if (i < 280) fixedLit.push(7)
+        else fixedLit.push(8)
+      }
+      var fixedDist = []
+      for (var i = 0; i < 30; i++) fixedDist.push(5)
+
+      function decompressBlock(lt, dt) {
+        while (true) {
+          var sym = decode(lt)
+          if (sym < 256) {
+            dst.push(sym)
+          } else if (sym === 256) {
+            break
+          } else {
+            var li = sym - 257
+            var length = lenBase[li] + bits(lenExtra[li])
+            var dsym = decode(dt)
+            var dist = distBase[dsym] + bits(distExtra[dsym])
+            var startIdx = dst.length - dist
+            for (var i = 0; i < length; i++) {
+              dst.push(dst[startIdx + i])
+            }
+          }
+        }
+      }
+
+      var last = false
+      while (!last) {
+        last = bits(1) === 1
+        var type = bits(2)
+
+        if (type === 0) {
+          bitBuf = 0
+          bitCnt = 0
+          var len = src[si] | (src[si + 1] << 8)
+          si += 4
+          for (var i = 0; i < len; i++) {
+            dst.push(src[si++])
+          }
+        } else if (type === 1) {
+          decompressBlock(buildTable(fixedLit), buildTable(fixedDist))
+        } else if (type === 2) {
+          var hlit = bits(5) + 257
+          var hdist = bits(5) + 1
+          var hclen = bits(4) + 4
+          var clOrder = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
+          var clLengths = []
+          for (var i = 0; i < 19; i++) clLengths.push(0)
+          for (var i = 0; i < hclen; i++) {
+            clLengths[clOrder[i]] = bits(3)
+          }
+          var clTree = buildTable(clLengths)
+
+          var allLengths = []
+          while (allLengths.length < hlit + hdist) {
+            var sym = decode(clTree)
+            if (sym < 16) {
+              allLengths.push(sym)
+            } else if (sym === 16) {
+              var rep = bits(2) + 3
+              var prev = allLengths[allLengths.length - 1]
+              for (var i = 0; i < rep; i++) allLengths.push(prev)
+            } else if (sym === 17) {
+              var rep = bits(3) + 3
+              for (var i = 0; i < rep; i++) allLengths.push(0)
+            } else if (sym === 18) {
+              var rep = bits(7) + 11
+              for (var i = 0; i < rep; i++) allLengths.push(0)
+            }
+          }
+
+          var litLengths = allLengths.slice(0, hlit)
+          var distLengths = allLengths.slice(hlit)
+          decompressBlock(buildTable(litLengths), buildTable(distLengths))
+        }
+      }
+
+      return dst
+    }
+
+    function parseZipEntries(data) {
+      var files = {}
+      var eocd = -1
+      for (var i = data.length - 22; i >= 0; i--) {
+        if (
+          data[i] === 0x50 &&
+          data[i + 1] === 0x4b &&
+          data[i + 2] === 0x05 &&
+          data[i + 3] === 0x06
+        ) {
+          eocd = i
+          break
+        }
+      }
+      if (eocd === -1) return null
+
+      var cdOffset =
+        data[eocd + 16] | (data[eocd + 17] << 8) | (data[eocd + 18] << 16) | (data[eocd + 19] << 24)
+      var cdEntries = data[eocd + 10] | (data[eocd + 11] << 8)
+
+      var pos = cdOffset
+      for (var i = 0; i < cdEntries; i++) {
+        if (
+          data[pos] !== 0x50 ||
+          data[pos + 1] !== 0x4b ||
+          data[pos + 2] !== 0x01 ||
+          data[pos + 3] !== 0x02
+        )
+          break
+
+        var compMethod = data[pos + 10] | (data[pos + 11] << 8)
+        var compSize =
+          data[pos + 20] | (data[pos + 21] << 8) | (data[pos + 22] << 16) | (data[pos + 23] << 24)
+        var nameLen = data[pos + 28] | (data[pos + 29] << 8)
+        var extraLen = data[pos + 30] | (data[pos + 31] << 8)
+        var commentLen = data[pos + 32] | (data[pos + 33] << 8)
+        var localOffset =
+          data[pos + 42] | (data[pos + 43] << 8) | (data[pos + 44] << 16) | (data[pos + 45] << 24)
+
+        var name = ''
+        for (var j = 0; j < nameLen; j++) {
+          name += String.fromCharCode(data[pos + 46 + j])
+        }
+
+        var lfh = localOffset
+        var localNameLen = data[lfh + 26] | (data[lfh + 27] << 8)
+        var localExtraLen = data[lfh + 28] | (data[lfh + 29] << 8)
+        var dataOffset = lfh + 30 + localNameLen + localExtraLen
+
+        var fileData = null
+        if (compMethod === 0) {
+          fileData = subarray(data, dataOffset, dataOffset + compSize)
+        } else if (compMethod === 8) {
+          var compData = subarray(data, dataOffset, dataOffset + compSize)
+          try {
+            fileData = inflateData(compData)
+          } catch (err) {
+            fileData = null
+          }
+        }
+
+        if (fileData) {
+          files[name] = fileData
+        }
+
+        pos += 46 + nameLen + extraLen + commentLen
+      }
+
+      return files
+    }
+
+    function extractSharedStrings(xmlStr) {
+      var strings = []
+      var siRegex = /<si[^>]*>([\s\S]*?)<\/si>/g
+      var match
+      while ((match = siRegex.exec(xmlStr)) !== null) {
+        var siContent = match[1]
+        var text = ''
+        var tRegex = /<t[^>]*>([\s\S]*?)<\/t>/g
+        var tMatch
+        while ((tMatch = tRegex.exec(siContent)) !== null) {
+          text += decodeXmlEntities(tMatch[1])
+        }
+        strings.push(text)
+      }
+      return strings
+    }
+
+    function getSheetFileMap(zipFiles) {
+      var sheetMap = {}
+      var wbXml = null
+      var relsXml = null
+
+      for (var name in zipFiles) {
+        if (name === 'xl/workbook.xml') wbXml = bytesToString(zipFiles[name])
+        if (name === 'xl/_rels/workbook.xml.rels') relsXml = bytesToString(zipFiles[name])
+      }
+
+      if (wbXml && relsXml) {
+        var rels = {}
+        var relRegex = /<Relationship\b[^>]*\/?>/g
+        var relMatch
+        while ((relMatch = relRegex.exec(relsXml)) !== null) {
+          var relTag = relMatch[0]
+          var idMatch = relTag.match(/Id="([^"]+)"/)
+          var targetMatch = relTag.match(/Target="([^"]+)"/)
+          if (idMatch && targetMatch) {
+            var target = decodeXmlEntities(targetMatch[1])
+            if (target.charAt(0) === '/') {
+              target = target.substring(1)
+            } else if (target.indexOf('xl/') !== 0) {
+              target = 'xl/' + target
+            }
+            rels[idMatch[1]] = target
+          }
+        }
+
+        var sheetRegex = /<sheet\b[^>]*\/?>/g
+        var sheetMatch
+        while ((sheetMatch = sheetRegex.exec(wbXml)) !== null) {
+          var sheetTag = sheetMatch[0]
+          var nameMatch = sheetTag.match(/name="([^"]+)"/)
+          var rIdMatch = sheetTag.match(/r:id="([^"]+)"/) || sheetTag.match(/\sId="([^"]+)"/)
+          if (nameMatch && rIdMatch && rels[rIdMatch[1]]) {
+            sheetMap[decodeXmlEntities(nameMatch[1]).toLowerCase()] = rels[rIdMatch[1]]
+          }
+        }
+      }
+
+      return sheetMap
+    }
+
+    function parseWorksheet(xmlStr, sharedStrings) {
+      var rows = []
+      var rowRegex = /<row[^>]*>([\s\S]*?)<\/row>/g
+      var rowMatch
+      while ((rowMatch = rowRegex.exec(xmlStr)) !== null) {
+        var rowContent = rowMatch[1]
+        var cells = []
+        var cRegex = /<c\b([^>]*)>([\s\S]*?)<\/c>|<c\b([^>]*)\/>/g
+        var cMatch
+        while ((cMatch = cRegex.exec(rowContent)) !== null) {
+          var attrs = cMatch[1] || cMatch[3] || ''
+          var content = cMatch[2] || ''
+
+          var rMatch = attrs.match(/r="([A-Z]+)\d+"/)
+          var colIdx = 0
+          if (rMatch) {
+            var colLetters = rMatch[1]
+            for (var ci = 0; ci < colLetters.length; ci++) {
+              colIdx = colIdx * 26 + (colLetters.charCodeAt(ci) - 64)
+            }
+            colIdx--
+          }
+
+          var tMatch = attrs.match(/t="([^"]+)"/)
+          var type = tMatch ? tMatch[1] : 'n'
+
+          var value = ''
+          if (type === 's') {
+            var vMatch = content.match(/<v[^>]*>([\s\S]*?)<\/v>/)
+            if (vMatch) {
+              var idx = parseInt(vMatch[1], 10)
+              value = sharedStrings[idx] || ''
+            }
+          } else if (type === 'str' || type === 'inlineStr') {
+            var tContent = content.match(/<t[^>]*>([\s\S]*?)<\/t>/)
+            if (tContent) {
+              value = decodeXmlEntities(tContent[1])
+            }
+          } else {
+            var vMatch2 = content.match(/<v[^>]*>([\s\S]*?)<\/v>/)
+            if (vMatch2) {
+              value = vMatch2[1]
+            }
+          }
+
+          while (cells.length <= colIdx) cells.push('')
+          cells[colIdx] = value
+        }
+
+        if (cells.length > 0) {
+          rows.push(cells)
+        }
+      }
+      return rows
+    }
+
+    function parseXlsx(fileBytes, config) {
+      var zipFiles = parseZipEntries(fileBytes)
+      if (!zipFiles) return { error: 'Formato XLSX invalido' }
+
+      var sharedStrings = []
+      for (var name in zipFiles) {
+        if (name === 'xl/sharedStrings.xml') {
+          sharedStrings = extractSharedStrings(bytesToString(zipFiles[name]))
+          break
+        }
+      }
+
+      var sheetMap = getSheetFileMap(zipFiles)
+      var sheetFile = null
+      var sheetNameUsed = null
+
+      if (config.sheet) {
+        sheetFile = sheetMap[config.sheet.toLowerCase()]
+        if (!sheetFile) {
+          var available = []
+          for (var sn in sheetMap) available.push(sn)
+          return {
+            error:
+              'Planilha "' +
+              config.sheet +
+              '" nao encontrada no arquivo ' +
+              config.label +
+              '. Abas disponiveis: ' +
+              available.join(', '),
+          }
+        }
+        sheetNameUsed = config.sheet
+      } else {
+        for (var name in zipFiles) {
+          if (name.match(/^xl\/worksheets\/sheet\d+\.xml$/)) {
+            sheetFile = name
+            break
+          }
+        }
+        if (!sheetFile) {
+          return { error: 'Nenhuma aba encontrada no arquivo ' + config.label }
+        }
+        sheetNameUsed = sheetFile
+      }
+
+      var sheetData = zipFiles[sheetFile]
+      if (!sheetData) {
+        return { error: 'Arquivo da planilha nao encontrado: ' + sheetFile }
+      }
+
+      var sheetXml = bytesToString(sheetData)
+      var rows = parseWorksheet(sheetXml, sharedStrings)
+
+      if (!rows || rows.length < 2) {
+        return {
+          error: 'Nenhum dado encontrado na aba "' + sheetNameUsed + '" do arquivo ' + config.label,
+        }
+      }
+
+      return { headerRow: rows[0], dataRows: rows.slice(1) }
+    }
+
     var parsed = []
 
     for (var pi = 0; pi < pendingFiles.length; pi++) {
@@ -265,66 +679,12 @@ routerAdd(
       var dataRows = null
 
       if (isXlsx) {
-        var binary = bytesToBinary(fileBytes)
-        var wb
-        try {
-          wb = XLSX.read(binary, { type: 'binary' })
-        } catch (err) {
-          return e.badRequestError(
-            'Erro ao ler arquivo XLSX: ' + config.label + ' - ' + (err.message || ''),
-          )
+        var xlsxResult = parseXlsx(fileBytes, config)
+        if (xlsxResult.error) {
+          return e.badRequestError(xlsxResult.error)
         }
-
-        var sheetName = config.sheet
-        var sheet = null
-
-        if (sheetName) {
-          sheet = wb.Sheets[sheetName]
-          if (!sheet) {
-            for (var si = 0; si < wb.SheetNames.length; si++) {
-              if (wb.SheetNames[si].toUpperCase() === sheetName.toUpperCase()) {
-                sheet = wb.Sheets[wb.SheetNames[si]]
-                break
-              }
-            }
-          }
-          if (!sheet) {
-            return e.badRequestError(
-              'Planilha "' +
-                config.sheet +
-                '" nao encontrada no arquivo ' +
-                config.label +
-                '. Abas disponiveis: ' +
-                wb.SheetNames.join(', '),
-            )
-          }
-        } else {
-          if (!wb.SheetNames || wb.SheetNames.length === 0) {
-            return e.badRequestError('Nenhuma aba encontrada no arquivo ' + config.label)
-          }
-          sheetName = wb.SheetNames[0]
-          sheet = wb.Sheets[sheetName]
-        }
-
-        var xlsxRows
-        try {
-          xlsxRows = XLSX.utils.sheet_to_json(sheet, {
-            header: 1,
-            raw: false,
-            defval: '',
-          })
-        } catch (err) {
-          return e.badRequestError('Erro ao processar aba "' + sheetName + '": ' + config.label)
-        }
-
-        if (!xlsxRows || xlsxRows.length < 2) {
-          return e.badRequestError(
-            'Nenhum dado encontrado na aba "' + sheetName + '" do arquivo ' + config.label,
-          )
-        }
-
-        headerRow = xlsxRows[0]
-        dataRows = xlsxRows.slice(1)
+        headerRow = xlsxResult.headerRow
+        dataRows = xlsxResult.dataRows
       } else {
         var text = bytesToString(fileBytes)
         if (text.charCodeAt(0) === 0xfeff) {
@@ -453,7 +813,27 @@ routerAdd(
                 }
               }
               if (field && field.type === 'date') {
-                value = formatDateValue(value)
+                var serialMatch = typeof value === 'string' ? value.match(/^(\d+(\.\d+)?)$/) : null
+                if (serialMatch) {
+                  var serial = parseFloat(serialMatch[1])
+                  if (serial > 1 && serial < 60000) {
+                    try {
+                      var dateObj = new Date(Math.round((serial - 25569) * 86400 * 1000))
+                      value =
+                        dateObj.getUTCFullYear() +
+                        '-' +
+                        pad2(dateObj.getUTCMonth() + 1) +
+                        '-' +
+                        pad2(dateObj.getUTCDate())
+                    } catch (de) {
+                      value = formatDateValue(value)
+                    }
+                  } else {
+                    value = formatDateValue(value)
+                  }
+                } else {
+                  value = formatDateValue(value)
+                }
               }
               if (field && field.type === 'number') {
                 var numVal = parseFloat(value.toString().replace(',', '.'))
